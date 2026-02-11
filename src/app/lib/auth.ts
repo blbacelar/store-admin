a0fcf54550fda7a06a65efc7b6c87184b72b1feb
Bruno Bacelar <blbacelar@gmail.com>
2026-02-10 20:39:39 -0700

    Fix production session detection by removing custom cookie configuration

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -59,15 +59,4 @@ export const authOptions: NextAuthOptions = {
         },
     },
     secret: process.env.NEXTAUTH_SECRET,
-    cookies: {
-        sessionToken: {
-            name: `next-auth.session-token`,
-            options: {
-                httpOnly: true,
-                sameSite: 'lax',
-                path: '/',
-                secure: process.env.NODE_ENV === 'production'
-            }
-        }
-    }
 };
